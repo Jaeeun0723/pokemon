547f9a46dc738b047ccf979cc75660d2e63a9566
Jaeeun0723 <lllbbch2@naver.com>
2025-06-12 18:16:12 +0900

    poke

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import { selectPokemonById } from "../RTK/selector"
+
+export default function Detail () {
+    const { pokemonId } = useParams()
+    const pokemon = useSelector(selectPokemonById(Number(pokemonId)))
+    console.log(pokemon)
+    return(
+    <div>
+        
+    </div>
+    )
+} 
+{/* <div>{pokemon.name} </div>
+        <div>{pokemon.description} </div>
+        <img src={pokemon.front}/> */}
